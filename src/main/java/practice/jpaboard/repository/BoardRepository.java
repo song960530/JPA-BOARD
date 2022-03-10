241a9110cc68d479e99b2749216b32ce120d1ad0
송문준 <song960530@naver.com>
2022-03-11 00:51:10 +0900

    feat: Board 엔티티를 생성하고 DataJpa동작을 확인하라

---
+++ src/main/java/practice/jpaboard/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package practice.jpaboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import practice.jpaboard.entity.Board;
+import practice.jpaboard.entity.Member;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByMember(Member member);
+}
--- src/test/java/practice/jpaboard/repository/BoardRepositoryTest.java
+++ src/test/java/practice/jpaboard/repository/BoardRepositoryTest.java
@@ -1,32 +1,44 @@
 package practice.jpaboard.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import practice.jpaboard.entity.Board;
 import practice.jpaboard.entity.Member;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberRepositoryTest {
+class BoardRepositoryTest {
+
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    BoardRepository boardRepository;
 
     @Test
     @DisplayName("스프링데이터JPA 기본동작을 확인한다")
     public void testSpringDataJpa() throws Exception {
         // given
         Member member = new Member("test", "test", "test", 11);
+        memberRepository.save(member);
+
+        Board board = new Board(member, "title", "content");
+        boardRepository.save(board);
 
         // when
-        memberRepository.save(member);
-        Member findMember = memberRepository.findByUserId("test");
+        List<Board> boardList = boardRepository.findByMember(member);
 
         // then
-        assertEquals(member.getUserId(), findMember.getUserId());
+        assertThat(boardList).extracting("member").containsOnly(member);
+        assertEquals("title", boardList.get(0).getTitle());
     }
-
 }

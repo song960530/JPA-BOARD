54f35134513d7131b9206b966662091f0269f223
송문준 <song960530@naver.com>
2022-03-21 13:05:34 +0900

    test

--- source/src/main/java/practice/jpaboard/domain/board/service/BoardService.java
+++ source/src/main/java/practice/jpaboard/domain/board/service/BoardService.java
@@ -76,6 +76,7 @@ public class BoardService {
         return ResultMessage.of(true, result, HttpStatus.OK);
     }
 
+    // test주석
     @Transactional
     public ResultMessage like(HttpServletRequest request, Long no) {
         Member member = memberService.findUserIdFromAuth();
